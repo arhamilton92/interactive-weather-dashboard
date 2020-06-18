793cd65b4c8aa1d9176bbde4de94897d95dd16ea
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-17 23:34:55 -0400

    filled out readme

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,12 @@ var APIKey = "4a5de0ef81ad062d6730642be9cf3b8e";
 
 // Here we are building the URL we need to query the database
 var queryURL = "";
+var queryURL2 = "";
 var arrayOfCities = [];
-var selectedCity = "Atlanta";
+var selectedCity = "";
 var city = "";
+var id = "";
+var country = ";"
 
 function searchCity() {  
     console.log(selectedCity)
@@ -22,21 +25,40 @@ function searchCity() {
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
-
+                console.log(response);
                 var K = response.main.temp;
-                var F = (K - 273.15) * 1.80 + 32
+                var F = (K - 273.15) * 1.80 + 32;
 
 
                 var cityEl = $("<h3>").attr("class", "mb-3").text(response.name);
                 var windEl = $("<p>").text("Speed: " + response.wind.speed + " MPH");
-                var humidityEl = $("<p>").text("Humidity " + response.main.humidity);
+                var humidityEl = $("<p>").text("Humidity: " + response.main.humidity);
                 var tempEl = $("<p>").text("Temperature: " + F);
 
+                id = response.id
+                country = response.sys.country
+
                 weather = $(".weather-info").append(cityEl, windEl, humidityEl, tempEl)
+
+                get5Day();
+
                 })
             }
     }
 }
+
+function get5Day() {
+    
+    queryURL2 = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?id=" + id + "&cnt=" + country + "&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    })
+}
+
 //capitalises the city name if not already capital
 function capitalizeCity() {
     selectedCity = selectedCity[0].toUpperCase() + selectedCity.slice(1);
@@ -57,7 +79,6 @@ function fromLocalStorage() {
     var getCity = localStorage.getItem("numberKey");
     if (getCity !== null) {
         getCity = getCity += 1;
-        console.log(getCity)
         $('.city-button').remove();
 
         for (i = 0; i < getCity || i === getCity; i++) {
@@ -66,14 +87,16 @@ function fromLocalStorage() {
             var cityButton = ($("<button>").attr("class", "btn btn-link white border city-button")).attr("id", "i").text(selectedCity);
             $(".city-button-list").prepend(cityButton);
             arrayOfCities.push(selectedCity);
+            console.log(selectedCity);
         }}
     }
-    console.log(selectedCity);
 }
 
 
 fromLocalStorage();
+selectedCity = "Atlanta";
 searchCity();
+
 
 $(".search-button").on("click", function() {
     selectedCity = $(".citysearch").val();
